4e07a3986d2976339909ae6e7dd16ca6897069c2
ashes4trees <stephsoni18@gmail.com>
2021-12-10 14:23:03 -0500

    Complete phase one

---
+++ widgets/frontend/widgets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Root() {
+    return (<div></div>);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<Root/>, document.getElementById('main'));
+});
+
